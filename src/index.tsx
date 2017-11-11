64e90763f15636c1533b4ed201427d0a3d6f7477
Alex Bepple <alex@bepple.de>
2017-11-11 23:08:54 +0100

    . use type information

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,7 @@ import * as React from 'react'
 import {render} from 'react-dom'
 import s from 's-js'
 
-const a = s.data([])
-
-const renderQuote = q => <li key={q.symbol}>{q.symbol + ': ' + q.price}</li>
-
-const App = ({quotes}) => <ul>
-    {quotes.map(renderQuote)}
-</ul>
-
-s.root(() => s(
-  () => render(<App quotes={a()} />, document.getElementById('root'))
-))
+const quotes = s.data([])
 
 interface Quote {
   symbol: string
@@ -22,9 +12,19 @@ interface Quote {
   timestamp: number
 }
 
+const renderQuote = (q: Quote) => <li key={q.symbol}>{q.symbol + ': ' + q.price}</li>
+
+const App = ({quotes}) => <ul>
+    {quotes.map(renderQuote)}
+</ul>
+
+s.root(() => s(
+  () => render(<App quotes={quotes()} />, document.getElementById('root'))
+))
+
 const toJson = x => x.json()
 const log = console.log.bind(console)
 
 fetch('https://wt-05c9272e8038f4b0552a40c8559e0b07-0.run.webtask.io/fx-rates')
 .then(toJson)
-.then(a)
+.then(quotes)
