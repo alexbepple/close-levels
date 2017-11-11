57c65b339a2178ecdce2414857b0a195ad6bbd18
Alex Bepple <alex@bepple.de>
2017-11-11 23:25:41 +0100

    . extract component

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,13 @@
 import * as React from 'react'
 import {render} from 'react-dom'
 import s from 's-js'
+import {CurrentQuotes} from './quotes'
 
-const quotes = s.data([])
-
-interface Quote {
-  symbol: string
-  price: number
-  bid: number
-  ask: number
-  timestamp: number
-}
-
-const renderQuote = (q: Quote) => <li key={q.symbol}>{q.symbol + ': ' + q.price}</li>
-
-const App = ({quotes}) => <ul>
-  {quotes.map(renderQuote)}
-</ul>
+const App = () => <div>
+  <h2>Rates</h2>
+  <CurrentQuotes />
+</div>
 
 s.root(() => s(
-  () => render(<App quotes={quotes()} />, document.getElementById('root'))
+  () => render(<App />, document.getElementById('root'))
 ))
-
-const toJson = x => x.json()
-const log = console.log.bind(console)
-
-fetch('https://wt-05c9272e8038f4b0552a40c8559e0b07-0.run.webtask.io/fx-rates')
-  .then(toJson)
-  .then(quotes)
---
+++ src/quotes.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import s from 's-js'
+
+const quotes = s.data([])
+
+interface Quote {
+  symbol: string
+  price: number
+  bid: number
+  ask: number
+  timestamp: number
+}
+
+const renderQuote = (q: Quote) => <li key={q.symbol}>{q.symbol + ': ' + q.price}</li>
+
+export const CurrentQuotes = () => <ul>{quotes().map(renderQuote)}</ul>
+
+fetch('https://wt-05c9272e8038f4b0552a40c8559e0b07-0.run.webtask.io/fx-rates')
+  .then(x => x.json())
+  .then(quotes)
