7699d28f92adbc6ed4b82d8ee059c302d172c748
Alex Bepple <alex@bepple.de>
2017-11-11 23:30:52 +0100

    . use Ramda

--- src/quotes.tsx
+++ src/quotes.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import s from 's-js'
+import * as r from 'ramda'
 
 const quotes = s.data([])
 
@@ -13,7 +14,7 @@ interface Quote {
 
 const renderQuote = (q: Quote) => <li key={q.symbol}>{q.symbol + ': ' + q.price}</li>
 
-export const CurrentQuotes = () => <ul>{quotes().map(renderQuote)}</ul>
+export const CurrentQuotes = () => <ul>{r.map(renderQuote, quotes())}</ul>
 
 fetch('https://wt-05c9272e8038f4b0552a40c8559e0b07-0.run.webtask.io/fx-rates')
   .then(x => x.json())
