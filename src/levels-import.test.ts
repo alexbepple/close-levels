35fccb85b666f292286c618a0bea7f2b7f106f85
Alex Bepple <alex@bepple.de>
2017-11-12 15:48:30 +0100

    % add test

--- src/levels-import.test.ts
+++ src/levels-import.test.ts
@@ -5,4 +5,7 @@ describe('Levels parser', () => {
   it('extracts level from line', () => {
     assertThat(getRate('foo at 1,2'), is(1.2))
   })
+  it("considers next number following 'at' level rate", () => {
+    assertThat(getRate('foo 1,0 bar at 1,1 baz 1,2'), is(1.1))
+  })
 })
