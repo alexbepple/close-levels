73c55313b1aa25dd2b23db69cbcc4fc559cce3b1
Alex Bepple <alex@bepple.de>
2017-11-12 17:31:36 +0100

    % break micro test in order to test deployment

--- src/levels.test.ts
+++ src/levels.test.ts
@@ -3,6 +3,6 @@ import {getDistanceInPercent} from './levels'
 
 describe('Levels presenter', () => {
   it('shows relative distance in percent', () => {
-    assertThat(getDistanceInPercent({price: 100, rate: 99}), is(1))
+    assertThat(getDistanceInPercent({price: 100, rate: 99}), is(2))
   })
 })
